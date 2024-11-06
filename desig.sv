2264219699574c6b48961b21c6ce71444e5725b2
Fyodor Amanov <20221500769@newuzbekistanuniversity.uz>
2024-11-06 10:45:05 +0500

    The two files with And, Nand and Xnor modules

--- desig.sv
+++ desig.sv
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module and_gate(
   input A,
   input B,
@@ -22,3 +23,28 @@ module xnor_gate (
   assign Y = ~(A^B);
 endmodule
 //
+=======
+module and_gate(
+  input A,
+  input B,
+  output Y
+);
+  assign Y = A&B;
+endmodule
+
+module nand_gate (
+  input A,
+  input B,
+  output Y
+);
+  assign Y = ~(A&B);
+endmodule
+
+module xnor_gate (
+  input A,
+  input B,
+  output Y
+);
+  assign Y = ~(A^B);
+endmodule
+>>>>>>> 01b4a0a (The two files with And, Nand and Xnor modules)
